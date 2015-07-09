5bc903ed34b68863e3c50e0a92a76399463a6a95
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-07-09 08:23:05 -0700

    Initialized nginx

--- init-vm.sh
+++ init-vm.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-yum install -y wireshark nginx java-1.8.0-openjdk
+#
+# This is the one time setup of the machine to get all necessary packages intalled
+#
+cat > /etc/yum.repos.d/nginx.repo << EOF
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/\$releasever/\$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+yum install -y wireshark nginx java-1.8.0-openjdk openssl
+service nginx start
+rm -rf /etc/nginx && cd /etc && ln -s /vagrant/test/etc/nginx
