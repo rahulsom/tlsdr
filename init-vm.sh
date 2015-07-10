0ccceb18d2470ec0508c85a346140ed4f217d167
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-07-09 21:48:16 -0700

    tlsdr in this vm causes go run
    Also nice aliases for curling

--- init-vm.sh
+++ init-vm.sh
@@ -2,6 +2,8 @@
 #
 # This is the one time setup of the machine to get all necessary packages intalled
 #
+
+# Add Nginx as a repo
 cat > /etc/yum.repos.d/nginx.repo << EOF
 [nginx]
 name=nginx repo
@@ -10,7 +12,20 @@ gpgcheck=0
 enabled=1
 EOF
 
-yum install -y wireshark nginx java-1.8.0-openjdk openssl git ruby rubygems
+# Install all packages required
+yum install -y wireshark nginx openssl git ruby rubygems #java-1.8.0-openjdk
+
+# Install thor for carb to work
 gem install thor
+
+# Softlink files necessary for working
 rm -rf /etc/nginx && cd /etc && ln -s /vagrant/test/etc/nginx
+rm /etc/hosts     && cd /etc && ln -s /vagrant/test/etc/hosts
+cd /etc/profile.d &&            ln -s /vagrant/test/etc/profile.d/aliases.sh
+
+# Start nginx
 service nginx start
+
+# Install go
+wget -q --no-check-certificate https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.4.2.linux-amd64.tar.gz
---
+++ test/etc/profile.d/aliases.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if [ $(echo $PATH | grep /usr/local/go/bin -c) = 0 ]; then
+    export PATH=$PATH:/usr/local/go/bin
+fi
+
+export PS1="\[$(tput bold)\]\[$(tput setaf 6)\]\t \[$(tput setaf 2)\][\[$(tput setaf 3)\]\u\[$(tput setaf 1)\]@\[$(tput setaf 3)\]\h \[$(tput setaf 6)\]\W\[$(tput setaf 2)\]]\[$(tput setaf 4)\]\\$ \[$(tput sgr0)\]"
+
+export CA_GOOD=/vagrant/test/goodca/caroot
+export  CA_BAD=/vagrant/test/badca/caroot
+
+alias tlsdr="go run /vagrant/src/tlsdr.go"
+alias _curl="curl -v"
+alias g_curl="_curl --cacert $CA_GOOD/cacert.pem"
+alias b_curl="_curl --cacert $CA_BAD/cacert.pem"
+alias ggcurl="g_curl -E $CA_GOOD/intrinsic/goodclient/cert.pem --key $CA_GOOD/intrinsic/goodclient/private.pem"
+alias bbcurl="b_curl -E $CA_BAD/intrinsic/badclient/cert.pem --key $CA_BAD/intrinsic/badclient/private.pem"
