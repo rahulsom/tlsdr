ebd6dfebb5bf023206f0e23e0cb1bc612c545313
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-07-15 10:28:56 -0700

    Capturing STDOUT and STDERR for the CURL

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -25,10 +25,11 @@ function stop() {
 
 function run() {
     echo "Testing $2 $4/$6 with $7"
-    capture $2-$4-$6-$1
+    CAPNAME=$2-$4-$6-$1
+    capture $CAPNAME
     curl --cacert /vagrant/test/$2/caroot/cacert.pem \
         -E /vagrant/test/$3/caroot/intrinsic/$4/cert.pem --key /vagrant/test/$5/caroot/intrinsic/$6/private.pem \
-        $7 > /dev/null 2>/dev/null
+        $7 > $OUTDIR/$CAPNAME.out 2> $OUTDIR/$CAPNAME.err
     if [ $? = 0 ]; then
         echo "Connected"
     else
