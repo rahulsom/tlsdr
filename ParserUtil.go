e2cf74cf7a579d6cb6a142326f7ba6d60212daf4
certifyTian <tlai@certifydatasystems.com>
2015-07-16 16:54:03 -0700

    Start to parse alert packet

--- ParserUtil.go
+++ ParserUtil.go
@@ -12,13 +12,13 @@ import (
 
 //TODO obviously not a main function, rename it to the caller
 func main() {
-	if handle, err := pcap.OpenOffline("data/goodca-goodclient-goodclient-trusted.pcap"); err != nil {
+	if handle, err := pcap.OpenOffline("data/goodca-goodclient-goodclient-bad.pcap"); err != nil {
 		panic(err)
 	} else {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		payloadPackets := producePayloadPackets(packetSource.Packets())
-		produceHandshakePackets(payloadPackets)
-
+		//produceHandshakePackets(payloadPackets)
+		produceAlertPackets(payloadPackets)
 	}
 }
 
@@ -63,8 +63,7 @@ func produceHandshakePackets(payloadPacs list.List) list.List {
 		switch handshake.HandshakeType {
 			case TLSHandshakeDecoder.HandshakeTypeClientHello: parseClientHello(handshake)
 			case TLSHandshakeDecoder.HandshakeTypeServerHello: parseServerHello(handshake)
-
-			default: //‰log.Printf("NOT covered")
+			default: //log.Printf("NOT covered")
 		}
 
 	}
@@ -98,6 +97,25 @@ func parseServerHello(hsp TLSHandshakeDecoder.TLSHandshake) TLSHandshakeDecoder.
 	var psh TLSHandshakeDecoder.TLSServerHello
 
 	return psh
+}
+
+func produceAlertPackets(payloadPacs list.List) list.List{
+	var alertPacs list.List
+	for e := payloadPacs.Front(); e != nil; e = e.Next() {
+		var p TLSHandshakeDecoder.TLSRecordLayer
+		pl := e.Value.([]byte)
+		err := TLSHandshakeDecoder.DecodeRecord(&p, pl); if err != nil {
+			panic(err)
+		} else {
+			if (len(p.Fragment) > 4 && p.ContentType == TLSHandshakeDecoder.TypeAlert) {
+				alertPacs.PushBack(p)
+			}
+		}
+	}
+	for e := alertPacs.Front(); e != nil; e = e.Next() {
+		log.Println("Alert data:", e)
+	}
+	return alertPacs
 }
 
 
