87abff3adfaa388b572d75cfce4b083bee3eb47a
Yuan Yao <yyao@humana.com>
2015-07-16 13:39:58 -0700

    init commit for reporting

---
+++ src/report2.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"text/template"
+	"os"
+	//"fmt"
+)
+
+const (
+	CLIENT = iota
+	SERVER = iota
+)
+
+type HandshakeProtocolStep struct {
+	Actor       int
+	Description string
+	Success     bool
+	Optional    bool
+	//Other attributes
+}
+
+type StepGroup struct {
+	Actor       int
+	steps       []HandshakeProtocolStep
+}
+
+
+func createStepGroups(steps []HandshakeProtocolStep)([]StepGroup) {
+	groups := make([]StepGroup, 0)
+	var pre HandshakeProtocolStep = steps[0]
+	var currGroup StepGroup = StepGroup{Actor: pre.Actor, steps: make([]HandshakeProtocolStep,0)}
+	for _, step := range steps {
+		if (step.Actor != pre.Actor) {
+			groups = append(groups, currGroup)
+			currGroup = StepGroup{Actor: step.Actor, steps: make([]HandshakeProtocolStep,0)}
+		}
+		currGroup.steps = append(currGroup.steps, step)
+		pre = step
+	}
+	groups = append(groups, currGroup)
+	return groups
+}
+
+func main() {
+	steps := make([]HandshakeProtocolStep,0)
+	step1 := HandshakeProtocolStep { Actor: CLIENT, Description: "Client Hello"}
+	step2 := HandshakeProtocolStep { Actor: SERVER, Description: "Server Hello"}
+	step3 := HandshakeProtocolStep { Actor: SERVER, Description: "Server Certificate"}
+	step4 := HandshakeProtocolStep { Actor: SERVER, Description: "Server Hello Done"}
+	steps = append(steps,step1)
+	steps = append(steps,step2)
+	steps = append(steps,step3)
+	steps = append(steps,step4)
+
+
+	groups := createStepGroups(steps)
+
+	//fmt.Println("There was an error:", steps)
+	tmpl, err := template.ParseFiles("template/txt/HandshakeProtocolDetails.txt")
+	if err != nil {
+		panic(err)
+	}
+	//tmpl, _ := template.New("detailsteps").Parse("{{.Actor}} items are made of {{.Description}}")
+	err = tmpl.Execute(os.Stdout, groups)
+	if err != nil {
+		panic(err)
+	}
+}
