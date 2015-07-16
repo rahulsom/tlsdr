2bb0fce09e19c3443d6de594008b016141a920d4
Yuan Yao <yyao@humana.com>
2015-07-16 15:40:02 -0700

    Connect view.go to tlsdr.go

--- View.go
+++ View.go
@@ -4,6 +4,7 @@ import (
 	"text/template"
 	"os"
 	//"fmt"
+	"container/list"
 )
 
 const (
@@ -38,6 +39,37 @@ func createStepGroups(steps []HandshakeProtocolStep)([]StepGroup) {
 		pre = step
 	}
 	groups = append(groups, currGroup)
+	return groups
+}
+
+func Visualize(data list.List, format string)([]byte) {
+	var result []byte
+	groups := getViewDataModel()
+	switch (format) {
+		case "txt": {
+			tmpl, err := template.ParseFiles("template/txt/HandshakeProtocolDetails.txt")
+			if err != nil { panic(err) }
+			err = tmpl.Execute(os.Stdout, groups)
+			if err != nil { panic(err) }
+		}
+	}
+	return result
+}
+
+//For now only test data
+func getViewDataModel()([]StepGroup) {
+	steps := make([]HandshakeProtocolStep,0)
+	step1 := HandshakeProtocolStep { Actor: CLIENT, Description: "Client Hello"}
+	step2 := HandshakeProtocolStep { Actor: SERVER, Description: "Server Hello"}
+	step3 := HandshakeProtocolStep { Actor: SERVER, Description: "Server Certificate"}
+	step4 := HandshakeProtocolStep { Actor: SERVER, Description: "Server Hello Done"}
+	steps = append(steps,step1)
+	steps = append(steps,step2)
+	steps = append(steps,step3)
+	steps = append(steps,step4)
+
+
+	groups := createStepGroups(steps)
 	return groups
 }
 
