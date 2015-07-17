4468c2a7a4125953a0caa4053b56d542e24cff45
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-07-16 20:37:55 -0700

    Added toString to domains

--- Domains.go
+++ Domains.go
@@ -1,5 +1,8 @@
 package main
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 type Connection struct {
 	success         bool
@@ -35,4 +38,13 @@ func NewEvent(code int) Event {
 
 func AddEvent(connection Connection, event Event) {
 	connection.events.PushBack(event)
+}
+
+func (event Event) String() string {
+	return fmt.Sprintf("Event{success: %t, type: '%s'}", event.success, event.eventType)
+}
+
+func (connection Connection) String() string {
+	return fmt.Sprintf("Connection{success: %t, failReason: '%s', src: '%s', dest: '%s', recommendations: %#v, events: %#v}",
+		connection.success, connection.failedReason, connection.srcHost, connection.destHost, connection.recommendations, connection.events)
 }
