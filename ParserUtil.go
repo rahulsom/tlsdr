a6dbeced0252d2322276b1f3f1cb78b0e64716c6
certifyTian <tlai@certifydatasystems.com>
2015-07-16 23:10:10 -0700

    Split the 2nd packet

--- ParserUtil.go
+++ ParserUtil.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket"
 	"github.com/certifyTian/TLSHandshakeDecoder"
 	_ "github.com/davecgh/go-spew/spew"
 	"log"
 	"container/list"
+	_ "errors"
 )
 
 
@@ -51,15 +52,14 @@ func producePayloadPackets(chanPacs chan gopacket.Packet) list.List {
 func produceHandshakePackets(payloadPacs list.List) list.List {
 	var handShakePacs list.List
 	for e := payloadPacs.Front(); e != nil; e = e.Next() {
-		var p TLSHandshakeDecoder.TLSRecordLayer
+		//var p TLSHandshakeDecoder.TLSRecordLayer
 		pl := e.Value.([]byte)
-		err := TLSHandshakeDecoder.DecodeRecord(&p, pl); if err != nil {
-			panic(err)
-		} else {
-			if (len(p.Fragment) > 4 && p.ContentType == TLSHandshakeDecoder.TypeHandshake) {
-				//handShakePacs
-				handShakePacs.PushBack(p)
+		packets := DecomposeRecordLayer(pl)
+		for e := packets.Front(); e != nil; e = e.Next() {
+			if(e.Value.(TLSHandshakeDecoder.TLSRecordLayer).ContentType == TLSHandshakeDecoder.TypeHandshake) {
+				handShakePacs.PushBack(e.Value)
 			}
+			//log.Println(e)
 		}
 	}
 
@@ -91,15 +91,44 @@ func parseClientHello(hsp TLSHandshakeDecoder.TLSHandshake) TLSHandshakeDecoder.
 
 func CreateEventsFromHSPackets(handShakePacs list.List) list.List {
 	var events list.List
-	for e := handShakePacs.Front(); e != nil; e = e.Next() {
-		log.Printf("Handshake data only:", e)
-		handshake := getHandShakeSegment(e.Value.(TLSHandshakeDecoder.TLSRecordLayer))
+	for el := handShakePacs.Front(); el != nil; el = el.Next() {
+		handshake := getHandShakeSegment(el.Value.(TLSHandshakeDecoder.TLSRecordLayer))
 		event := NewEvent(int(handshake.HandshakeType))
 		events.PushBack(event)
+		log.Printf("Created Event:", event)
 	}
 	return events
 }
 
+
+
+func DecomposeRecordLayer(data []byte) list.List {
+	if len(data) < 5 {
+		return list.List{}
+	}
+	log.Println("Calling......")
+	var handshakelist list.List
+	total := uint16(len(data))
+	var offset uint16 = 0
+
+	for (offset < total) {
+		var p TLSHandshakeDecoder.TLSRecordLayer
+		p.ContentType = uint8(data[0+offset])
+		p.Version = uint16(data[1+offset])<<8 | uint16(data[2+offset])
+		p.Length = uint16(data[3+offset])<<8 | uint16(data[4+offset])
+		p.Fragment = make([]byte, p.Length)
+		l := copy(p.Fragment, data[5+offset:5+p.Length+offset])
+		handshakelist.PushBack(p)
+		log.Println("Length: ", p.Length)
+		offset += 5+p.Length
+		log.Print("Type:  ", p.Fragment[0])
+		if l < int(p.Length) {
+			fmt.Errorf("Payload to short: copied %d, expected %d.", l, p.Length)
+		}
+	}
+
+	return handshakelist
+}
 
 
 
