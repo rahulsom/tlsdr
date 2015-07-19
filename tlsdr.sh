cf019845914766cd253592e36f3668de7ecbf448
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-07-19 14:21:37 -0700

    Fixed bash script references

--- tlsdr.sh
+++ tlsdr.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 cd /root/tlsdr
-gom build
 ls -1tr data/*.pcap | sed -e "s/.pcap$//g" | sed -s "s/^data.//g" \
-    | xargs -n 1 -I {} ./tlsdr -i /vagrant/data/{}.pcap -o /usr/share/nginx/html/{}/html -f html
+    | xargs -n 1 -I {} gom run tlsdr/*.go -i /vagrant/data/{}.pcap -o /usr/share/nginx/html/{}/html -f html
 ls -1tr data/*.pcap | sed -e "s/.pcap$//g" | sed -s "s/^data.//g" \
-    | xargs -n 1 -I {} ./tlsdr -i /vagrant/data/{}.pcap -o /usr/share/nginx/html/{}/txt -f txt
+    | xargs -n 1 -I {} gom run tlsdr/*.go -i /vagrant/data/{}.pcap -o /usr/share/nginx/html/{}/txt -f txt
